caef62b0987ad284ff0f1723d0c39d7f6a65414f
H3ARTgg <nomaxismax@gmail.com>
2023-09-19 23:05:40 +0300

    initial commit

---
+++ ARShooter/Presentation/GameModule/GameViewController.swift
@@ -0,0 +1,61 @@
+import UIKit
+import SceneKit
+import ARKit
+
+final class GameViewController: UIViewController {
+    private let sceneView = ARSCNView()
+    private let mainScene = SCNScene()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    private func configureViewController() {
+        setupSceneView(sceneView, with: mainScene)
+    }
+    
+    private func setupSceneView(_ sceneView: ARSCNView, with scene: SCNScene) {
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.scene = scene
+        sceneView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(sceneView)
+        
+        NSLayoutConstraint.activate([
+            sceneView.topAnchor.constraint(equalTo: view.topAnchor),
+            sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+extension GameViewController: ARSCNViewDelegate {
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
---
+++ ARShooter/Presentation/GameModule/GameViewModel.swift
@@ -0,0 +1,3 @@
+final class GameViewModel {
+    
+}
