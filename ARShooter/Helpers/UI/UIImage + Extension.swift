407013d681b3abe7a9c75726660431e5d45781bf
H3ARTgg <nomaxismax@gmail.com>
2023-09-20 01:00:41 +0300

    feat: UIViewController and UIImage extensions

---
+++ ARShooter/Helpers/UI/UIImage + Extension.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIImage {
+    static let background = UIImage(named: "background.jpg") ?? UIImage()
+    static let menuBackground = UIImage(named: "menuBackground.jpg") ?? UIImage()
+}
