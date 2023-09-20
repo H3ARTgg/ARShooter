aab485d7d5c871cf93aa655c71780ed3933bb500
H3ARTgg <nomaxismax@gmail.com>
2023-09-20 16:08:47 +0300

    refactor: UIImage extension

--- ARShooter/Helpers/UI/UIImage + Extension.swift
+++ ARShooter/Helpers/UI/UIImage + Extension.swift
@@ -3,4 +3,6 @@ import UIKit
 extension UIImage {
     static let background = UIImage(named: "background.jpg") ?? UIImage()
     static let menuBackground = UIImage(named: "menuBackground.jpg") ?? UIImage()
+    static let sphereTexture = UIImage(named: "sphereTexture") ?? UIImage()
+    static let crosshair = UIImage(named: "crosshair") ?? UIImage()
 }
