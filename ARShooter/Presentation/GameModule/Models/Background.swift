781d8c7a17f815bf5304914468b01750ec4ceac9
H3ARTgg <nomaxismax@gmail.com>
2023-09-20 22:26:16 +0300

    refactor: add background node name

--- ARShooter/Presentation/GameModule/Models/Background.swift
+++ ARShooter/Presentation/GameModule/Models/Background.swift
@@ -4,6 +4,7 @@ final class Background {
     static func makeBackgroundNode() -> SCNNode {
         let sphereNode = SCNNode(geometry: SCNSphere(radius: 10))
         let material = SCNMaterial()
+        sphereNode.name = "background"
         material.cullMode = .front
         material.diffuse.contents = UIImage.background
         sphereNode.geometry?.materials = [material]
