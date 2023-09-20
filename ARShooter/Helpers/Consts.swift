c326a7f09d300ce2d12f509a12f58ecce4e09e0b
H3ARTgg <nomaxismax@gmail.com>
2023-09-21 00:22:55 +0300

    feat: add Consts

---
+++ ARShooter/Helpers/Consts.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+final class Consts {
+    // Target
+    static let targetSpawnInterval: Double = 1
+    static let targetAnimationDuration: Double = 15
+    
+    // Background
+    static let backgroundRadius: CGFloat = 10
+    
+    // Timers
+    static let countdownToStart: Double = 3
+    static let timeLeft: Double = 10
+}
