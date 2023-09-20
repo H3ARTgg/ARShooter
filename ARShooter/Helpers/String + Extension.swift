01993a5812573b40b8f39fc9c80f8bd878820f16
H3ARTgg <nomaxismax@gmail.com>
2023-09-21 02:57:16 +0300

    refactor: new localization

--- ARShooter/Helpers/String + Extension.swift
+++ ARShooter/Helpers/String + Extension.swift
@@ -2,10 +2,26 @@ import Foundation
 
 enum LocaleKeys: String {
     case play = "play"
-    case statistics = "statistics"
+    case leaderboards = "leaderboards"
+    case backToMenu = "back_to_menu"
+    case saveToLeaderboards = "save_to_leaderboards"
+    case timeLeft = "time_left"
+    case gameOver = "game_over"
+    case score = "score"
+    case seconds = "seconds"
+    case totalShots = "totalShots"
+    case totalHits = "totalHits"
 }
 
 extension String {
     static let play = NSLocalizedString(LocaleKeys.play.rawValue, comment: "")
-    static let statistics = NSLocalizedString(LocaleKeys.statistics.rawValue, comment: "")
+    static let leaderboards = NSLocalizedString(LocaleKeys.leaderboards.rawValue, comment: "")
+    static let backToMenu = NSLocalizedString(LocaleKeys.backToMenu.rawValue, comment: "")
+    static let saveToLeaderboards = NSLocalizedString(LocaleKeys.saveToLeaderboards.rawValue, comment: "")
+    static let timeLeft = NSLocalizedString(LocaleKeys.timeLeft.rawValue, comment: "")
+    static let gameOver = NSLocalizedString(LocaleKeys.gameOver.rawValue, comment: "")
+    static let score = NSLocalizedString(LocaleKeys.score.rawValue, comment: "")
+    static let seconds = NSLocalizedString(LocaleKeys.seconds.rawValue, comment: "")
+    static let totalShots = NSLocalizedString(LocaleKeys.totalShots.rawValue, comment: "")
+    static let totalHits = NSLocalizedString(LocaleKeys.totalHits.rawValue, comment: "")
 }
