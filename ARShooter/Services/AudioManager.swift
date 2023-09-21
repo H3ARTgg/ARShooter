894e3a7e7fc2ef8b6ae722a1c54aa6f64a4bd004
H3ARTgg <nomaxismax@gmail.com>
2023-09-21 20:45:28 +0300

    feat: add AudioManager

--- ARShooter/Services/AudioManager.swift
+++ ARShooter/Services/AudioManager.swift
@@ -1,5 +1,34 @@
 import AVKit
 
 final class AudioManager {
-    func play
+    static let shared = AudioManager()
+    var players: [AVAudioPlayer] = []
+    
+    func playCountdown() {
+        playSoundFor(Consts.countdownSound)
+    }
+    
+    func playLaserShot() {
+        playSoundFor(Consts.laserShotSound)
+    }
+    
+    func playTargetSpawn() {
+        playSoundFor(Consts.targetSpawnSound)
+    }
+    
+    private func playSoundFor(_ soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: ".mp3") else { return }
+        guard let player = try? AVAudioPlayer(contentsOf: url) else { return }
+        players.append(player)
+        if soundName == Consts.targetSpawnSound {
+            player.volume = 0.5
+        }
+        player.play()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [weak self] in
+            guard let self else { return }
+            self.players.removeAll {
+                $0 == player
+            }
+        }
+    }
 }
