42fc7ab11841ea1460fa52d5efecf9f6fb28042f
H3ARTgg <nomaxismax@gmail.com>
2023-09-22 18:01:56 +0300

    feat: add TutorialVC and TutorialVM

---
+++ ARShooter/Presentation/TutorialModule/TutorialViewController.swift
@@ -0,0 +1,136 @@
+import UIKit
+import ARKit
+import SceneKit
+import Combine
+
+final class TutorialViewController: UIViewController {
+    private var shootButton = UIButton()
+    private var cancellables: Set<AnyCancellable> = []
+    private lazy var tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap))
+    private lazy var exitButton = UIButton.systemButton(with: .xMark, target: self, action: #selector(didTapExit))
+    private let sceneView = ARSCNView()
+    private let mainScene = SCNScene()
+    private let crosshairImageView = UIImageView()
+    private let viewModel: TutorialViewModelProtocol
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    init(viewModel: TutorialViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: .none, bundle: .main)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc
+    private func didTap(_ touch: UITapGestureRecognizer) {
+        let sceneView = touch.view as! SCNView
+        let touchLocation = touch.location(in: sceneView)
+        guard let node = getNodeFrom(sceneView, for: touchLocation), node.name == TapMeText.name else { return }
+        checkForShooting()
+    }
+    
+    @objc
+    private func didTapShoot() {
+        viewModel.playShotAudio()
+        let sceneView = sceneView as SCNView
+        let touchLocation = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        guard let node = getNodeFrom(sceneView, for: touchLocation), node.name == SphereTarget.name else { return }
+        node.removeFromParentNode()
+        viewModel.getTarget()
+    }
+    
+    @objc
+    private func didTapExit() {
+        dismiss(animated: true)
+    }
+    
+    private func configureViewController() {
+        setupSceneView(sceneView, with: mainScene)
+        addNodes()
+        setupExitButton(exitButton)
+        binds()
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        viewModel.getTarget()
+        viewModel.getVideoScreen()
+    }
+    
+    private func checkForShooting() {
+        if view.subviews.contains(where: { $0 == crosshairImageView }) {
+            crosshairImageView.removeFromSuperview()
+            shootButton.removeFromSuperview()
+        } else {
+            setupCrosshairImageView(crosshairImageView)
+            setupShootButton(&shootButton, action: #selector(didTapShoot))
+            shootButton.alpha = 1
+            shootButton.isEnabled = true
+        }
+    }
+    
+    private func binds() {
+        viewModel.targetSpawnPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] target in
+                guard let self else { return }
+                self.sceneView.scene.rootNode.addChildNode(target)
+            }
+            .store(in: &cancellables)
+        
+        viewModel.newVideoScreenPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] node in
+                guard let self else { return }
+                if let oldNode = self.sceneView.scene.rootNode.childNodes.first(where: { $0.name == VideoScreen.name }) {
+                    oldNode.removeFromParentNode()
+                    self.sceneView.scene.rootNode.addChildNode(node)
+                } else {
+                    self.sceneView.scene.rootNode.addChildNode(node)
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: UI
+private extension TutorialViewController {
+    func addNodes() {
+        let background = Background.makeBackgroundNode()
+        let descriptionText = DescriptionText.populate(at: SCNVector3(-0.8, -0.4, -2))
+        let tapMeText = TapMeText.populate(at: SCNVector3(-0.8, -0.6, -2))
+        
+        [background, descriptionText, tapMeText].forEach {
+            sceneView.scene.rootNode.addChildNode($0)
+        }
+    }
+    
+    func setupExitButton(_ button: UIButton) {
+        button.setTitle(nil, for: .normal)
+        button.tintColor = .green
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            button.widthAnchor.constraint(equalToConstant: 40),
+            button.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
---
+++ ARShooter/Presentation/TutorialModule/TutorialViewModel.swift
@@ -0,0 +1,63 @@
+import SceneKit
+import Combine
+
+protocol TutorialViewModelProtocol {
+    var targetSpawnPublisher: AnyPublisher<SCNNode, Never> { get }
+    var newVideoScreenPublisher: AnyPublisher<SCNNode, Never> { get }
+    func getTarget()
+    func getVideoScreen()
+    func playShotAudio()
+}
+
+final class TutorialViewModel: TutorialViewModelProtocol {
+    var targetSpawnPublisher: AnyPublisher<SCNNode, Never> {
+        targetSpawnSubject.eraseToAnyPublisher()
+    }
+    var newVideoScreenPublisher: AnyPublisher<SCNNode, Never> {
+        newVideoScreenSubject.eraseToAnyPublisher()
+    }
+    private var loopScreenTimer = Timer()
+    private let targetSpawnSubject = PassthroughSubject<SCNNode, Never>()
+    private let newVideoScreenSubject = PassthroughSubject<SCNNode, Never>()
+    
+    func playShotAudio() {
+        AudioManager.shared.playLaserShot()
+    }
+    
+    func getTarget() {
+        let randomY = Float.random(in: -0.8...0)
+        let position = SCNVector3(-1.2, randomY, -2)
+        let target = SphereTarget.populateSphere(at: position)
+        target.addAnimation(getRotatingAnimation(), forKey: "")
+        target.filters = getBloom()
+        targetSpawnSubject.send(target)
+    }
+    
+    func getVideoScreen() {
+        loopScreenTimer = Timer.scheduledTimer(timeInterval: 14, target: self, selector: #selector(didUpdateTimer), userInfo: nil, repeats: true)
+        let screen = VideoScreen.populate(at: SCNVector3(1, -0.3, -2))
+        newVideoScreenSubject.send(screen)
+    }
+    
+    @objc
+    private func didUpdateTimer() {
+        let screen = VideoScreen.populate(at: SCNVector3(1, -0.3, -2))
+        newVideoScreenSubject.send(screen)
+    }
+    
+    private func getBloom() -> [CIFilter]? {
+        let bloomFilter = CIFilter(name:"CIBloom")!
+        bloomFilter.setValue(5.0, forKey: "inputIntensity")
+        bloomFilter.setValue(20.0, forKey: "inputRadius")
+        return [bloomFilter]
+    }
+    
+    private func getRotatingAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "rotation")
+        animation.duration = 5
+        animation.fromValue = SCNVector4(0, 0, 0, 0)
+        animation.toValue = SCNVector4(0, 0, 1, CGFloat(2 * Double.pi))
+        animation.repeatCount = .infinity
+        return animation
+    }
+}
